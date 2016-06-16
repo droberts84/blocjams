f67b8025685ffdcc6d98ac435a49ad8c20dcf124
David Roberts <drob@drob.net>
2016-06-15 23:59:16 -0400

    CP 23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,29 @@
+var pointsArray = document.getElementsByClassName('point');
+
 var animatePoints = function() {
-
-    var points = document.getElementsByClassName('point');
-
     var revealPoint = function(index) {
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+      pointsArray[index].style.opacity = 1;
+      pointsArray[index].style.transform = "scaleX(1) translateY(0)";
+      pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
+      pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
 
-    for(var i = 0; i < points.length; i++) {
+    for(var i = 0; i < pointsArray.length; i++) {
       revealPoint(i);
     }
+};
+
+window.onload = function() {
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  if (window.innerHeight > 950) {
+     animatePoints(pointsArray);
+   }
+
+  window.addEventListener('scroll', function(event) {
+    if( document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
 };
